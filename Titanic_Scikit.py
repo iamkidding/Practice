22b976245299389a68a9d2b0a9f3b77acd2108bd
iamkidding <iamkindding@163.com>
2018-01-07 00:01:56 +0800

    尝试按年龄分组，但使用pd.concat和pd.merge后数据集会变成empty

--- Titanic_Scikit.py
+++ Titanic_Scikit.py
@@ -12,6 +12,8 @@ test_raw = pd.read_csv(open('C:/Song-Code/Practice/test.csv'))
 # data_raw = data_raw.replace(['male', 'female'], [1, 0])
 # test_raw = test_raw.replace(['male','female'], [1, 0])
 # 将Sex，Pclass设置成虚拟变量
+# 不设置成虚拟变量，而是直接将sex改成0,1值，最后的泛化结果较差，在kaggle为95%，
+# 使用虚拟变量，得出的svm模型可以到44%
 dum_sex = pd.get_dummies(data_raw['Sex'], prefix="Sex")
 dum_pclass = pd.get_dummies(data_raw['Pclass'], prefix="Pclass")
 data_raw = pd.concat([data_raw, dum_sex, dum_pclass], axis=1)
@@ -43,6 +45,14 @@ age_scale_para_test = scaler.fit(test_raw["Age"].reshape(-1, 1))
 test_raw["Age_scaled"] = scaler.fit_transform(test_raw["Age"].reshape(-1, 1), age_scale_para_test)
 fare_scale_para_test = scaler.fit(test_raw["Fare"].reshape(-1, 1))
 test_raw["Fare_scaled"] = scaler.fit_transform(test_raw["Fare"].reshape(-1, 1), fare_scale_para_test)
+
+# 将年龄分组，分为18岁以下，18-60,60以上三组
+age_bins = [0, 12, 100]
+age_group = ['Child', 'Adult']
+Age_cat = pd.cut(data_raw["Age"], age_bins, labels=age_group)
+data_raw_age = pd.merge(data_raw, pd.DataFrame(Age_cat)) #为什么这一步完成后data_raw_age为空
+# age_dum = pd.get_dummies(data_raw['Age_cat'], prefix='Age')
+# data_raw = pd.concat([data_raw, age_dum], axis=1)
 
 # 选取要包含在模型中的特征
 train = data_raw.drop(["PassengerId", "Survived", "Name", "Ticket", "Cabin", "Embarked", "Sex",
