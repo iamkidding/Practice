fa14415877b9ff92e9628be0af08d52d8dd9f439
iamkidding <iamkindding@163.com>
2018-04-25 22:09:41 +0800

    在每层都添加了dropout，并随机选取了1000张图片进行了翻转。还在训练，不知是否解决了过拟合问题

--- Dog_Breed_Identification.py
+++ Dog_Breed_Identification.py
@@ -33,7 +33,6 @@ def read_resize_test(file_route, width, height):
     for file in files:
         image = cv2.imread(file_route +"/" +file)
         image = cv2.resize(image, (width, height))
-        # image = tf.reshape(image, [width, height, 3])
         image = np.array(image)
         image = image - image.mean() # 零均值
         image /= np.std(image) # 归一化
@@ -42,23 +41,42 @@ def read_resize_test(file_route, width, height):
         i += 1
     return data
 
+def image_augmentation(data, labels, num): # 暂时只实现水平翻转
+    for i in range(num):
+        random_index = np.random.randint(len(data))
+        # method = np.random.randint(1)
+        # if method == 0:
+        image_aug = cv2.flip(data[random_index], 1)
+        # else:
+        #     image_aug = cv2.
+        image_aug = np.reshape(image_aug, (1, 64, 64, 3))
+        data = np.concatenate((data, image_aug))
+        if i == 0: # 第一轮之后，labels的type由DataFrame变为npdarray
+            labels = np.concatenate((labels, labels.loc[random_index,:].values.reshape(1, 2)))
+        else:
+            labels = np.concatenate((labels, np.reshape(labels[random_index,:],(1, 2))))
+
+    return data, labels
+
 def one_hot(labels):
     labels_one_hot = pd.get_dummies(labels)
     return labels_one_hot
 
-def next_batch(data, labels, num):
+def next_batch(data, labels, num, is_df):
     len = data.shape[0]
     indices = np.arange(len)
     np.random.shuffle(indices)
     indices = indices[:num]
     train_mini = data[indices]
-    labels_mini = one_hot(labels["breed"])
-    labels_mini = labels_mini.loc[indices,]
+    # 训练的labels是nparray， 验证集的是df
+    if is_df is not True:
+        labels_mini = one_hot(labels[:, 1])
+    else:
+        labels_mini = one_hot(labels["breed"])
+    #经过one_hot函数后变为df
+    labels_mini = labels_mini.loc[indices]
 
     return train_mini, labels_mini
-
-def next_batch_to_predict(test, num):
-    pass
 
 def weights_init(name,shape):
     # return tf.Variable(tf.truncated_normal(shape, stddev=0.01))
@@ -86,49 +104,51 @@ for i in range(len(test_id)):
     test_id[i] = test_id[i].split(".")[0]
 
 train, val = read_resize_train_val(train_route, 64, 64)
+train, train_labels = image_augmentation(train, train_labels, 1000)
 test = read_resize_test(test_route, 64, 64)
 
 x = tf.placeholder("float", shape=[None, 64, 64, 3])
 y = tf.placeholder("float", shape=[None, 120])
+keep_prob = tf.placeholder("float")  # dropout的比例
+is_training = tf.placeholder("bool")
 
 # 设计神经网络结构
-is_training = tf.placeholder("bool")
+if is_training is not False:
+    x = tf.nn.dropout(x, keep_prob) # 在训练时将x drop掉一半
+bnx = tf.layers.batch_normalization(x, training=is_training)
 # 卷积第一层
 w_conv1 = weights_init("w_conv1", [5, 5, 3, 32])
-# w_conv1 = tf.get_variable("w_conv1", shape=[5, 5, 3, 32], initializer=tf.contrib.layers.xavier_initializer())
 b_conv1 = bias_init([32])
 bn1 = tf.layers.batch_normalization(conv2d(x, w_conv1) + b_conv1, training=is_training)
 h_conv1 = tf.nn.relu(bn1)
-h_pool1 = max_pool(h_conv1)
+h_conv1_drop = tf.nn.dropout(h_conv1, keep_prob)
+h_pool1 = max_pool(h_conv1_drop)
 # 第二层
 w_conv2 = weights_init("w_conv2", [5, 5, 32, 64])
-# w_conv2 = tf.get_variable("w_conv2", shape=[5, 5, 32, 64], initializer=tf.contrib.layers.xavier_initializer())
 b_conv2 = bias_init([64])
 bn2 = tf.layers.batch_normalization(conv2d(h_pool1, w_conv2) + b_conv2, training=is_training)
 h_conv2 = tf.nn.relu(bn2)
-h_pool2 = max_pool(h_conv2)
+h_conv2_drop = tf.nn.dropout(h_conv2, keep_prob)
+h_pool2 = max_pool(h_conv2_drop)
 # 第三层
 w_conv3 = weights_init("w_conv3", [5, 5, 64, 128])
-# w_conv3 = tf.get_variable("w_conv3", shape=[5, 5, 64, 128], initializer=tf.contrib.layers.xavier_initializer())
 b_conv3 = bias_init([128])
 bn3 = tf.layers.batch_normalization(conv2d(h_pool2, w_conv3) + b_conv3, training=is_training)
 h_conv3 = tf.nn.relu(bn3)
-h_pool3 = max_pool(h_conv3)
+h_conv3_drop = tf.nn.dropout(h_conv3, keep_prob=keep_prob)
+h_pool3 = max_pool(h_conv3_drop)
 # 全连接层第一层
 w_fc1 = weights_init("w_fc1", [128*8*8, 1024])
-# w_fc1 = tf.get_variable("w_fc1", shape=[128*8*8, 1024], initializer=tf.contrib.layers.xavier_initializer())
 b_fc1 = bias_init([1024])
 h_pool3_flat = tf.reshape(h_pool3, [-1, 128*8*8])
 h_fc1 = tf.nn.relu(tf.matmul(h_pool3_flat, w_fc1) + b_fc1)
-keep_prob = tf.placeholder("float")  # dropout的比例
 h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
 # 第二层
 w_fc2 = weights_init("w_fc2", [1024, 120])
-# w_fc2 = tf.get_variable("w_fc2", shape=[1024, 120], initializer=tf.contrib.layers.xavier_initializer())
 b_fc2 = bias_init([120])
 
-y_ = tf.nn.softmax(tf.matmul(h_fc1_drop, w_fc2) + b_fc2) # 预测时候用
-# y_ = tf.matmul(h_fc1_drop, w_fc2) + b_fc2  # 训练时候用
+# y_ = tf.nn.softmax(tf.matmul(h_fc1_drop, w_fc2) + b_fc2) # 预测时候用
+y_ = tf.matmul(h_fc1_drop, w_fc2) + b_fc2  # 训练时候用
 
 # 损失函数
 # cross_entropy = -tf.reduce_sum(y_*tf.log(y_+1e-10))
@@ -144,19 +164,19 @@ accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
 
 with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
-    # for i in range(16000):
-    #     train_mini, labels_mini = next_batch(train, train_labels, 50)
-    #     val_mini, vl_mini = next_batch(val, val_labels, 10) # 验证集
-    #     if i % 100 == 0: # 每100次进行一次验证
-    #         train_accuracy = accuracy.eval(feed_dict={x: train_mini, y: labels_mini, keep_prob: 1.0, is_training: True})
-    #         val_accuracy = accuracy.eval(feed_dict={x: val_mini, y: vl_mini, keep_prob: 1.0, is_training: False})
-    #         loss = cross_entropy.eval(feed_dict={x: train_mini, y: labels_mini, keep_prob: 1.0, is_training: True})
-    #         print("step %d, training acc %g, validation acc %g, loss %g" % (i, train_accuracy, val_accuracy, loss))
-    #     train_step.run(feed_dict={x:train_mini, y:labels_mini, keep_prob:0.5, is_training:True})
-    #     if i % 1000 == 0: # 每1000次存一下模型
-    #         saver = tf.train.Saver()
-    #         model_path = "C:\Song-Code\model\Dog_Breed_Identification.ckpt"
-    #         save_path = saver.save(sess, model_path)
+    for i in range(16000):
+        train_mini, labels_mini = next_batch(train, train_labels, 50, False)
+        if i % 100 == 0: # 每100次进行一次验证
+            val_mini, vl_mini = next_batch(val, val_labels, 50, True)  # 验证集
+            train_accuracy = accuracy.eval(feed_dict={x: train_mini, y: labels_mini, keep_prob: 1.0, is_training: True})
+            val_loss = cross_entropy.eval(feed_dict={x: val_mini, y: vl_mini, keep_prob: 1.0, is_training: False})
+            loss = cross_entropy.eval(feed_dict={x: train_mini, y: labels_mini, keep_prob: 1.0, is_training: True})
+            print("step %d, training acc %g, validation loss %g, loss %g" % (i, train_accuracy, val_loss, loss))
+        train_step.run(feed_dict={x:train_mini, y:labels_mini, keep_prob:0.5, is_training:True})
+        if i % 1000 == 0: # 每1000次存一下模型
+            saver = tf.train.Saver()
+            model_path = "C:\Song-Code\model\Dog_Breed_Identification1.ckpt"
+            save_path = saver.save(sess, model_path)
 
     saver = tf.train.Saver()
     model_path = "C:\Song-Code\model\Dog_Breed_Identification.ckpt"
